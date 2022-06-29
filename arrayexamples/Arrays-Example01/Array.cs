c4536f04abf938c180061bb00d49b6e9e5522c5c
Kevin <kbutchm@gmail.com>
2022-06-29 08:08:49 -0400

    Add project files.

---
+++ arrayexamples/Arrays-Example01/Array.cs
@@ -0,0 +1,118 @@
+﻿namespace Arrays_Example01
+{
+    public class Array
+    {
+        private int[] items;
+        private int count;
+
+        public Array(int length)
+        {
+            items = new int[length];
+        }
+
+        public void Insert(int number)
+        {
+            // construct dynamic array
+            ResizeIfRequired();
+
+            items[count++] = number;
+        }
+
+        private void ResizeIfRequired()
+        {
+            if (items.Length == count)
+            {
+                int[] newItems = new int[count * 2];
+
+                for (int i = 0; i < count; i++)
+                {
+                    newItems[i] = items[i];
+                }
+                items = newItems;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            // validate the index
+            if(index < 0 || index >= count)
+                throw new ArgumentOutOfRangeException("index");
+
+            for (int i = index; i < count; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            count--;
+
+        }
+
+        public int IndexOf(int number)
+        {
+            // O(1) - best case scenario (first index)
+            // 0(n) - worst case scenario (needs to iterate through entire array)
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i] == number)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public int Max()
+        {
+            int max = 0;
+            for (int i = 0; i < count; i++)
+                if(items[i] > max)
+                    max = items[i];
+            
+            return max;
+        }
+
+        public Array Intersect(Array other)
+        {
+            var intersection = new Array(count);
+
+            foreach (var item in items)
+                if (other.IndexOf(item) >= 0)
+                    intersection.Insert(item);
+            
+            return intersection;
+        }
+
+        public void Reverse()
+        {
+            int[] newitems = new int[count];
+
+            for (int i = 0; i < count; i++)
+                newitems[i] = items[count - i - 1];
+
+            items = newitems;
+        }
+
+        public void InsertAt(int item, int index)
+        {
+            if (index < 0 || index > count)
+                throw new ArgumentOutOfRangeException();
+
+            ResizeIfRequired();
+
+            for (int i = count - 1; i >= index; i--)
+                items[i + 1] = items[i];
+
+            items[index] = item;
+            count++;
+
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ arrayexamples/Arrays-Example01/Program.cs
@@ -0,0 +1,31 @@
+﻿var input = 3;
+
+var numbers = new Arrays_Example01.Array(input);
+numbers.Insert(10);
+numbers.Insert(20);
+numbers.Insert(30);
+numbers.Insert(40);
+numbers.Insert(50);
+numbers.print();
+
+numbers.RemoveAt(3);
+numbers.print();
+
+Console.WriteLine(numbers.IndexOf(10));
+Console.WriteLine(numbers.IndexOf(40));
+
+Console.WriteLine();
+Console.WriteLine(numbers.Max());
+Console.WriteLine();
+
+numbers.InsertAt(100, 1);
+numbers.print();
+
+numbers.Reverse();
+numbers.print();
+
+var otherInput = 1;
+var otherArray = new Arrays_Example01.Array(otherInput);
+otherArray.Insert(40);
+otherArray.print();
+
