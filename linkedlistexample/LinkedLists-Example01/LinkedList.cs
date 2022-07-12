3379601b4fd39e81e8d4186bc3d87cf03d4b5442
Kevin <kmolloy.consulting@gmail.com>
2022-07-12 18:39:35 -0400

    linked list addfirst, addlast, indexof, contians, size, removefirst, removelast

---
+++ linkedlistexample/LinkedLists-Example01/LinkedList.cs
@@ -0,0 +1,157 @@
+﻿namespace LinkedLists_Example01
+{
+    public class LinkedList
+    {
+        private class Node
+        {
+            internal int value;
+            internal Node next;
+
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        private Node first;
+        private Node last;
+        private int size;
+
+        // addFirst
+        public void AddFirst(int item)
+        {
+            var node = new Node(item);
+
+            if (IsEmpty())
+                first = last = node;
+            else
+            {
+                node.next = first;
+                first = node;
+            }
+            size++;
+        }
+
+        // addLast
+        public void AddLast(int item)
+        {
+            var node = new Node(item);
+
+            if (IsEmpty())
+                first = last = node;
+            else
+            {
+                last.next = node;
+                last = node;
+            }
+            size++;
+        }       
+        
+        // removeFirst
+        public void RemoveFirst()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            if (first == last)
+                first = last = null;
+            else
+            {
+                var second = first.next;
+                first.next = null;
+                first = second;
+            }
+            size--;
+        }
+
+        // removeLast
+        public void RemoveLast()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            if (first == last)
+                first = last = null;
+            else
+            {
+                var previous = GetPrevious(last);
+                last = previous;
+                last.next = null;
+            }
+            size--;
+        }
+
+        // contains
+        public bool Contains(int item) => IndexOf(item) != -1;
+
+        // size -> O(1)
+        public int Size() => size;
+
+        // convert to array
+        public int[] ToArray()
+        {
+            int[] array = new int[size];
+            var current = first;
+            var index = 0;
+            while(current != null)
+            {
+                array[index++] = current.value;
+                current = current.next;
+            }
+
+            return array;
+        }
+
+        public void Reverse()
+        {
+            if (IsEmpty()) return;
+
+            var previous = first;
+            var current = first.next;
+
+            while(current != null)
+            {
+                var next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+            }
+
+            last = first;
+            last.next = null;
+            first = previous;
+        }
+
+        // indexOf
+        public int IndexOf(int item)
+        {
+            int index = 0;
+            var current = first;
+            while(current != null)
+            {
+                if (current.value == item) return index;
+                current = current.next;
+                index++;
+            }
+
+            return -1;
+        }
+
+        #region private methods
+        private bool IsEmpty() => first == null;
+
+        private Node GetPrevious(Node node)
+        {
+            var current = first;
+            while(current != null)
+            {
+                if(current.next == node) return current;
+                current = current.next;
+            }
+
+            return null;
+        }
+        #endregion
+
+    }
+}
--- linkedlistexample/LinkedLists-Example01/Program.cs
+++ linkedlistexample/LinkedLists-Example01/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var list = new LinkedLists_Example01.LinkedList();
+Console.WriteLine($"Size: {list.Size()}");
+list.AddLast(10);
+list.AddLast(20);
+list.AddLast(30);
+
+list.AddFirst(5);
+
+Console.WriteLine();
+var array = list.ToArray();
+PrintArray(array);
+
+list.Reverse();
+array = list.ToArray();
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine($"Size: {list.Size()}");
+
+Console.WriteLine(list.IndexOf(10));
+Console.WriteLine(list.IndexOf(1000));
+Console.WriteLine(list.Contains(20));
+Console.WriteLine(list.Contains(1000));
+
+list.RemoveFirst();
+list.RemoveLast();
+Console.WriteLine($"Size: {list.Size()}");
+
+array = list.ToArray();
+PrintArray(array);
+
+Console.WriteLine();
+
+static void PrintArray(int[] array)
+{
+    List<int> numbers = array.ToList();
+    numbers.ForEach(Console.WriteLine);
+}
