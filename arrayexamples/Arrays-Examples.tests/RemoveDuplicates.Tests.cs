4a892c6c302bb07cf45e5f65062889dc9ad72f6c
Kevin <kmolloy.consulting@gmail.com>
2022-07-27 12:31:27 -0400

    add arrays remove duplicates

---
+++ arrayexamples/Arrays-Examples.tests/RemoveDuplicates.Tests.cs
@@ -0,0 +1,37 @@
+﻿using RemoveDuplicates_Example;
+
+namespace Arrays_Examples.Tests
+{
+    public class RemoveDuplicatesTests
+    {
+        private RemoveDuplicates _removeDuplicates;
+
+        [SetUp]
+        public void Setup()
+        {
+            _removeDuplicates = new RemoveDuplicates();
+        }
+
+        [Test]
+        [TestCase(new object[] {1, 0, 1, 0}, new object[] {1, 0})]
+        [TestCase(new object[] { "The", "Big", "Hat" }, new object[] { "The", "Big", "Hat" })]
+        [TestCase(new object[] { "The", "Big", "Hat", "Big" }, new object[] { "The", "Big", "Hat" })]
+        public void RemoveDuplicatesWithLinq_ReturnArray(object[] array, object[] expectedResult)
+        {
+            var result = _removeDuplicates.RemoveDupsWithLinq(array);
+
+            Assert.That(result, Is.EquivalentTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(new object[] { 1, 0, 1, 0 }, new object[] { 1, 0 })]
+        [TestCase(new object[] { "The", "Big", "Hat" }, new object[] { "The", "Big", "Hat" })]
+        [TestCase(new object[] { "The", "Big", "Hat", "Big" }, new object[] { "The", "Big", "Hat" })]
+        public void RemoveDuplicates_ReturnArray(object[] array, object[] expectedResult)
+        {
+            var result = _removeDuplicates.RemoveDupsWithLinq(array);
+
+            Assert.That(result, Is.EquivalentTo(expectedResult));
+        }
+    }
+}
---
+++ arrayexamples/RemoveDuplicates-Example/RemoveDuplicates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates_Example
+{
+    public class RemoveDuplicates
+    {
+        public object[] RemoveDupsWithLinq(object[] items)
+        {
+            return items.Distinct().ToArray();
+        }
+
+        public object[] RemoveDups(object[] items)
+        {
+            var tempList = new List<object>();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (!tempList.Contains(items[i]))
+                    tempList.Add(items[i]);
+            }
+
+            return tempList.ToArray();
+        }
+    }
+}
