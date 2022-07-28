5dca544f372e0a0aedbbe43f11e83d4d873f43f2
Kevin <kmolloy.consulting@gmail.com>
2022-07-28 12:04:59 -0400

    add remove deuplicates with for loop

--- arrayexamples/RemoveDuplicates-Example/RemoveDuplicates.cs
+++ arrayexamples/RemoveDuplicates-Example/RemoveDuplicates.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RemoveDuplicates_Example
+﻿namespace RemoveDuplicates_Example
 {
     public class RemoveDuplicates
     {
