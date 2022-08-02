8bddaa713c5e9630d60f4543451faffe056839a7
Kevin <kmolloy.consulting@gmail.com>
2022-08-01 20:33:11 -0400

    add stack balanced expression and tests

---
+++ stacks/BalancedExpression-Example/BalancedExpression.cs
@@ -0,0 +1,50 @@
+﻿namespace BalancedExpression_Example
+{
+    public class BalancedExpression
+    {
+        private List<char> leftBrackets = new() { '(', '<', '[', '{' };
+        private List<char> rightBrackets = new() { ')', '>', ']', '}' };
+
+        public bool IsBalanced(string input)
+        {
+            if(string.IsNullOrWhiteSpace(input))
+                throw new ArgumentNullException();
+
+            Stack<char> stack = new();
+
+            foreach (char item in input.ToCharArray())
+            {
+                if(IsLeftBracket(item))
+                    stack.Push(item);
+
+                if (IsRightBracket(item))
+                {
+                    if(stack.Count == 0)
+                        return false;
+
+                    var top = stack.Pop();
+                    if (!IsBracketsMatch(top, item)) return false;
+                }
+
+            }
+
+            return stack.Count == 0;
+        }
+
+        public bool IsLeftBracket(char item)
+        {
+            return leftBrackets.Contains(item);
+        }
+
+        public bool IsRightBracket(char item)
+        {
+            return rightBrackets.Contains(item);
+
+        }
+        public bool IsBracketsMatch(char left, char right)
+        {
+            return leftBrackets.IndexOf(left) == rightBrackets.IndexOf(right);
+        }
+
+    }
+}
---
+++ stacks/Stacks-Example01.Tests/BalancedExpressionTests.cs
@@ -0,0 +1,76 @@
+﻿using BalancedExpression_Example;
+
+namespace Stacks_Example01.Tests
+{
+    public class BalancedExpressionTests
+    {
+        private BalancedExpression _balancedExpression;
+
+        [SetUp]
+        public void Setup()
+        {
+            _balancedExpression = new BalancedExpression();
+        }
+
+        [Test]
+        [TestCase("(2 + 1)", true)]
+        [TestCase("{2 + 1}", true)]
+        [TestCase("(2 + 1", false)]
+        [TestCase(")2 + 1(", false)]
+        [TestCase("(2 + 1>", false)]
+        public void GetBalancedExpression_ReturnBoolean(string input, bool expectedResult)
+        {
+            var result = _balancedExpression.IsBalanced(input);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void GetBalancedExpression_InvalidError_ThrowArgumentNullException(string input)
+        {
+            Assert.That(() => _balancedExpression.IsBalanced(input), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        [TestCase('(', true)]
+        [TestCase('{', true)]
+        [TestCase('<', true)]
+        [TestCase('[', true)]
+        [TestCase('*', false)]
+        public void GetIsLeftBracket_ReturnBoolean(char input, bool expectedResult)
+        {
+            var result = _balancedExpression.IsLeftBracket(input);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(')', true)]
+        [TestCase('}', true)]
+        [TestCase('>', true)]
+        [TestCase(']', true)]
+        [TestCase('*', false)]
+        public void GetIsRightBracket_ReturnBoolean(char input, bool expectedResult)
+        {
+            var result = _balancedExpression.IsRightBracket(input);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase("(", ")", true)]
+        [TestCase('{', '}', true)]
+        [TestCase('<', '>', true)]
+        [TestCase('[', ']', true)]
+        [TestCase('(', '>',false)]
+        public void GetIsBracketsMatch_ReturnBoolean(char left, char right, bool expectedResult)
+        {
+            var result = _balancedExpression.IsBracketsMatch(left, right);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
