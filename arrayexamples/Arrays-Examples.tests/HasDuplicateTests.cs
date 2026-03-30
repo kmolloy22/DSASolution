f70684ad83a9e48e8ee0d957e03948c715f7b883
Kevin Molloy (KVMO) <kvmo@demant.com>
2026-03-30 10:52:24 -0400

    Add HasDuplicate-Example project with tests and docs

---
+++ arrayexamples/Arrays-Examples.tests/HasDuplicateTests.cs
@@ -0,0 +1,40 @@
+﻿using HasDuplicate_Example;
+
+namespace Arrays_Examples.Tests;
+
+public class HasDuplicateTests
+{
+    private HasDuplicate _hasDuplicate;
+
+    public HasDuplicateTests()
+    {
+        _hasDuplicate = new HasDuplicate();
+    }
+
+    [Test]
+    [TestCase(new int[] { 1, 2, 3, 4 }, false)]
+    [TestCase(new int[] { 1, 2, 3, 1 }, true)]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, false)]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1 }, true)]
+    public void HasDuplicateNumbersBruteForce_ReturnBool(int[] nums, bool expectedResult)
+    {
+        var result = _hasDuplicate.HasDuplicateNumbersBruteForce(nums);
+        Assert.That(result, Is.EqualTo(expectedResult));
+
+    }
+
+    [Test]
+    [TestCase(new int[] { 1, 2, 3, 4 }, false)]
+    [TestCase(new int[] { 1, 2, 3, 1 }, true)]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, false)]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1 }, true)]
+    [TestCase(new int[] { }, false)]
+    [TestCase(new int[] { 42 }, false)]
+    [TestCase(new int[] { 5, 5, 5 }, true)]
+    [TestCase(new int[] { 9, 9, 1, 2, 3 }, true)]
+    public void HasDuplicateNumbersHashSet_ReturnBool(int[] nums, bool expectedResult)
+    {
+        var result = _hasDuplicate.HasDuplicateNumbersHashSet(nums);
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
---
+++ arrayexamples/HasDuplicate-Example/HasDuplicate.cs
@@ -0,0 +1,34 @@
+﻿namespace HasDuplicate_Example;
+
+public class HasDuplicate
+{
+    public bool HasDuplicateNumbersBruteForce(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public bool HasDuplicateNumbersHashSet(int[] nums)
+    {
+        var seenNumbers = new HashSet<int>();
+        foreach (var num in nums)
+        {
+            if (!seenNumbers.Add(num))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
